dc251149945cf1203314475fad2a67461945bd8e
ksole <so0771fe@gmail.com>
2025-07-16 22:21:44 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('med-dose-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/app.js',
+        '/manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
